88a4f3512ae4d5ff8b90f654af053b63501f03fc
harsh-sakariya <harshs@prominentpixel.com>
2021-09-25 18:30:50 +0530

    change some variable type let to const

--- Final Exercise/exercise3/exercise3.js
+++ Final Exercise/exercise3/exercise3.js
@@ -1,15 +1,15 @@
-let arr = ['hello', 'Two11', '11Five111' ,'One1', 'pro1m1in1en1t', 'H1a1r1s1h'];
+const arr = ['hello', 'Two11', '11Five111' ,'One1', 'pro1m1in1en1t', 'H1a1r1s1h'];
 
 console.log(filterAndReplace(arr, '1', 'e'));
 
 function filterAndReplace(strArr, search, replace){
-    let  filteredArr = strArr.filter((element)=>{
+    const  filteredArr = strArr.filter((element)=>{
         let count = 0;
         element.split('').map(ch => ch==='1' ? count++ : '');
         return count>=3;
     })
     
-    let resultArr = filteredArr.map((element)=>{
+    const resultArr = filteredArr.map((element)=>{
         return element.split(search).join(replace);
     })
 
